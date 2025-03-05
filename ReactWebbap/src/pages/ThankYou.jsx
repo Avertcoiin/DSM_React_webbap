7ba6586625190304482987038c8dd8e4dba2b470
Avertcoiin <vertiz.138863@e.unavarra.es>
2025-03-05 16:50:00 +0100

    Navegación básica funcional

--- ReactWebbap/src/pages/ThankYou.jsx
+++ ReactWebbap/src/pages/ThankYou.jsx
@@ -3,16 +3,18 @@
 import { useNavigate } from 'react-router-dom';
 
 function ThankYou() {
-
   const navigate = useNavigate();
 
   return (
-    <div className="thank-you">
+    <div className="thank-you text-center mt-5">
       <h2>¡Gracias por tu compra!</h2>
       <p>Tu pedido ha sido realizado con éxito.</p>
-      <button className="btn btn-success btn-lg mb-2" onClick={navigate("/")}>REALIZAR UN NUEVO PEDIDO</button>
+      <button className="btn btn-success btn-lg mt-3" onClick={() => navigate("/")}>
+        REALIZAR UN NUEVO PEDIDO
+      </button>
     </div>
-  )
+  );
 }
 
-export default ThankYou
+export default ThankYou;
+
