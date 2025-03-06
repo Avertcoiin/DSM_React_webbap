42157ed86e95b8eed639bbe7a329b49f2b828bbf
Avertcoiin <vertiz.138863@e.unavarra.es>
2025-03-06 10:59:13 +0100

    Intentando conectar con firebase Segurmaente lo dejemos para mañana.

---
+++ ReactWebbap/src/firebase_conf/firebase.jsx
@@ -0,0 +1,29 @@
+// src/firebase_config/firebase.js
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIPoki5tnSMZJq6CmTaEtM1inSRY7CxBg",
+  authDomain: "dsm-vertiz-muro-proyecto-react.firebaseapp.com",
+  databaseURL: "https://dsm-vertiz-muro-proyecto-react-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "dsm-vertiz-muro-proyecto-react",
+  storageBucket: "dsm-vertiz-muro-proyecto-react.firebasestorage.app",
+  messagingSenderId: "814138972562",
+  appId: "1:814138972562:web:4af704634b4ab52492894b",
+  measurementId: "G-LY3KY0JT3T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// Obtener la base de datos
+const db = getDatabase(app);
+
+export { db };
