624b9f8a35ae753039959436530319c1055cc5ab
Avertcoiin <vertiz.138863@e.unavarra.es>
2025-03-06 10:15:12 +0100

    Burbujita hecha y botón ver envios "tonto" creado

--- ReactWebbap/src/components/ui/Header.jsx
+++ ReactWebbap/src/components/ui/Header.jsx
@@ -30,12 +30,13 @@ function Header() {
         <input type="text" placeholder="Buscar..." className="form-control mx-3" style={{ maxWidth: "300px" }} />
       </div>
       <div className="d-flex align-items-center">
-        <div className="cart mx-3">
+        <div className="cart mx-3 position-relative">
           <img src={carrito} alt="Carrito" style={{ height: "40px" }} />
-          <span className="cart-count" style={{ color: "white" }}>
-            {totalCantidad} {/* Muestra la cantidad total de productos */}
+          <span className="badge rounded-circle bg-white text-dark fw-bold position-absolute top-0 start-100 translate-middle border border-dark">
+            {totalCantidad}
           </span>
         </div>
+
         <div className="total mx-3 text-light">
           <span>Total: {totalPrecio.toFixed(2)}€</span> {/* Muestra el total en precio */}
         </div>
